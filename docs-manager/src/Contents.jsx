0fc787e2d1ef9335dfd1b77fc23cfa9b2b56407b
Debapriya Das <debapriyadas73@gmail.com>
2022-12-29 23:47:46 +0530

    folder structure

---
+++ docs-manager/src/Contents.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+
+export const Contents = () => {
+  return (
+    <>
+      <Flex pos="sticky" left="5" h="95vh" w="200px" boxShadow="">
+        <Flex></Flex>
+      </Flex>
+    </>
+  );
+};
--- docs-manager/src/components/Contents.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Contents = () => {
-  return <div>Contents</div>;
-};
