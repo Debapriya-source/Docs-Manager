a527a2d948e4e5f65c56887d48bb826b6b459872
Debapriya Das <debapriyadas73@gmail.com>
2022-12-29 19:57:19 +0530

    contents

--- docs-manager/src/App.jsx
+++ docs-manager/src/App.jsx
@@ -3,18 +3,18 @@ import { Topics } from "./components/Topics";
 import { Routes, Route } from "react-router-dom";
 import { About } from "./components/About";
 import { Box } from "@chakra-ui/react";
-import { Contents } from "./components/contents/Contents";
+import { Contents } from "./components/Contents";
 function App() {
   // console.log("App");
   return (
     <Box bgColor="rgb(41, 41, 41)" height="100%">
       <Navbar />
-    <Routes>
-      <Route path="/" element={<Topics />} />
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contents" element={<Contents />} />
-      <Route path="*" element={<h1>404 Not Found</h1>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Topics />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contents" element={<Contents />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
       {/* <Topics /> */}
     </Box>
   );
--- docs-manager/src/components/contents/Base.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Base = () => {
-  return (
-    <div>Base</div>
-  )
-}
